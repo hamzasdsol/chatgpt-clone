d99ae07c2c9071bdd73c3c4d3ddfa8221908fec2
Hamza Abid <52952681+hamzasdsol@users.noreply.github.com>
2024-08-05 12:06:00 +0500

    commit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,3 +1,4 @@
+
 import React from 'react';
 import { FaPaperclip } from 'react-icons/fa';
 
@@ -5,12 +6,12 @@ const Footer = () => {
   return (
     <div className='bg-white border-t border-gray-200 rounded-full'>
       <div className="relative max-w-7xl mx-auto py-4 px-4 sm:px-6 lg:px-8">
-        <div className="relative">
+        <div className="relative rounded-full">
           <textarea
-            className="w-full border-2 border-black rounded-md rounded-full py-2 px-12 resize-none focus:outline-none focus:ring-2 focus:ring-blue-500"
+            className="w-full border-2 border-black rounded-full py-2 px-12 resize-none focus:outline-none focus:ring-2 focus:ring-blue-500"
             placeholder="Message ChatGPT..."
           ></textarea>
-          <div className="absolute top-1/2 left-4 transform -translate-y-1/2 text-gray-500">
+          <div className="absolute top-1/2 left-4 transform -translate-y-4 text-gray-500 rounded-full">
             <FaPaperclip />
           </div>
         </div>
@@ -20,6 +21,7 @@ const Footer = () => {
 }
 
 export default Footer;
+
 
 
 
